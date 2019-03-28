8aa84b8ce4d50fc89d4b683ce6ba2d22965c57b3
Kibian <kevcass01@yahoo.fr>
2019-03-28 11:24:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Quotes from "./Quotes";
+import Lamp from "./Lamp";
 
 class App extends Component {
+  constructor(working){
+    super(working);
+    this.state = {
+      working: false
+    };
+    this.handleClick= () => {
+      this.setState({ working: !this.state.working });
+    };
+  }
   render() {
+    const work = this.state.working ? 'true' : 'false';
+    const turn = this.state.working ? 'App-logo' : 'App-logo2';
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} className={turn} alt="logo" />
+          <h1 className="App-title">Simpsons Quotes</h1>
         </header>
+        <Lamp on />
+        <Lamp />
+        <button
+            onClick={this.handleClick} 
+            className={work}>{work.toUpperCase()}
+        </button>
+        <Quotes />
       </div>
     );
   }
